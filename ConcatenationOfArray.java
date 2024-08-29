82e9fd645427926f42927e9dbdfb0f746af3d1f4
Rakib Islam <109210735+CodeOrganizer@users.noreply.github.com>
2024-08-30 00:06:38 +0530

    Create ConcatenationOfArray.java

---
+++ ConcatenationOfArray.java
@@ -0,0 +1,36 @@
+package ProblemSolving;
+/**
+ * @author iraki
+ */
+public class ConcatenationOfArray {
+    
+    public static int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int arr[]=new int[2*n];
+//        for(int i=0;i<arr.length;i++)
+//        {
+//            if(i>=nums.length)
+//            {
+//                int index=i-nums.length;
+//                arr[i]=nums[index];
+//            }
+//            else
+//                arr[i]=nums[i];
+//        }
+//        for(int i=0;i<n;i++)
+//        {
+//            arr[i]=nums[i];
+//            arr[i+n]=nums[i];
+//        }
+        System.arraycopy(nums, 0, arr, 0, n);
+        System.arraycopy(nums, 0, arr, n, n );      //src,src pos,dest,dest pos,length (length means how long will be copled)
+        return arr;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3};
+        int arr2[]=getConcatenation(arr);
+        for(int x:arr2)
+            System.out.print(x+" ");
+    }
+
+}
