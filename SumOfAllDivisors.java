ced9794bf94d0b553d18b985799b3a5f2198900b
Rakib Islam <109210735+Koragg98@users.noreply.github.com>
2024-09-07 17:27:15 +0530

    Create SumOfAllDivisors.java

---
+++ SumOfAllDivisors.java
@@ -0,0 +1,38 @@
+//Given a positive integer N., The task is to find the value of Σi from 1 to N F(i) where function F(i) for the number i is defined as the sum of all divisors of i.
+//https://www.geeksforgeeks.org/problems/sum-of-all-divisors-from-1-to-n4738/1
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+            Solution ob = new Solution();
+            long ans  = ob.sumOfDivisors(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution{
+    static long sumOfDivisors(int N){
+        // code here
+        long sum=0l;
+        for(int n=1;n<=N;n++)
+        {
+           sum+=(N/n)*n;
+        }
+        return sum;
+    }
+}
